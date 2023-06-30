cb0903097cf731abaece477ec8e5d0cb91d41a63
Itzelll <104791502+Itzelll@users.noreply.github.com>
2023-06-29 20:32:28 -0600

    Create lesson-1.py

---
+++ cursos-IML/lesson-1.py
@@ -0,0 +1,73 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Read the data
+X_full = pd.read_csv('../input/train.csv', index_col='Id')
+X_test_full = pd.read_csv('../input/test.csv', index_col='Id')
+
+# Remove rows with missing target, separate target from predictors
+X_full.dropna(axis=0, subset=['SalePrice'], inplace=True)
+y = X_full.SalePrice
+X_full.drop(['SalePrice'], axis=1, inplace=True)
+
+# To keep things simple, we'll use only numerical predictors
+X = X_full.select_dtypes(exclude=['object'])
+X_test = X_test_full.select_dtypes(exclude=['object'])
+
+# Break off validation set from training data
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,
+                                                      random_state=0)
+
+#-----------------1
+# Fill in the line below: How many rows are in the training data?
+num_rows = 1168
+
+# Fill in the line below: How many columns in the training data
+# have missing values?
+num_cols_with_missing = 3
+
+# Fill in the line below: How many missing entries are contained in 
+# all of the training data?
+tot_missing = 212+6+58
+
+
+#-------------------2
+# Fill in the line below: get names of columns with missing values
+cols_with_missing = [col for col in X_train.columns
+                     if X_train[col].isnull().any()] # Your code here
+
+# Fill in the lines below: drop columns in training and validation data
+reduced_X_train = X_train.drop(cols_with_missing, axis=1)
+reduced_X_valid = X_valid.drop(cols_with_missing, axis=1)
+
+
+#---------------------3
+# Fill in the lines below: imputation
+inputer = SimpleImputer() # Your code here
+imputed_X_train = pd.DataFrame(inputer.fit_transform(X_train))
+imputed_X_valid = pd.DataFrame(inputer.transform(X_valid))
+
+# Fill in the lines below: imputation removed column names; put them back
+imputed_X_train.columns = X_train.columns
+imputed_X_valid.columns = X_valid.columns
+
+
+#------------------4
+# Preprocessed training and validation features
+final_imputer = SimpleImputer(strategy='median')
+final_X_train = pd.DataFrame(final_imputer.fit_transform(X_train))
+final_X_valid = pd.DataFrame(final_imputer.transform(X_valid))
+
+# Imputation removed column names; put them back
+final_X_train.columns = X_train.columns
+final_X_valid.columns = X_valid.columns
+
+
+#-----------------5
+# Fill in the line below: preprocess test data
+final_X_test = pd.DataFrame(final_imputer.transform(X_test))
+
+# Fill in the line below: get test predictions
+preds_test = model.predict(final_X_test)
+
+
